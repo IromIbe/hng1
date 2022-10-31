9207f1036835591a4790add5c209534bfff9573f
IromIbe <iromibe@outlook.com>
2022-10-31 03:40:49 -0700

    made a few design changes to match figma design

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className='flex sm:flex-row flex-col  justify-between border-t pt-6'>
+    <div className='flex sm:flex-row flex-col px-2 xsm:px-0  justify-between border-t pt-6'>
       <div className='zuri sm:my-0 my-1'>
         <img src='/zuri.svg' alt='zuri_logo' />
       </div>
       <div className='sm:my-0 my-1'>
-        <span className='sm:text-base text-sm text-[#667085]'>
+        <span className='font-[400] sm:text-base text-sm text-[#667085]'>
           HNG Internship 9 Frontend Task
         </span>
       </div>
